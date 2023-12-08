b609017a6819969f320837c1a0a919d70d77234b
Aleenarajan <150989676+Aleenarajan@users.noreply.github.com>
2023-12-08 11:56:06 +0530

    Add files via upload

---
+++ S1/ADS/OUTPUT.c
@@ -0,0 +1,126 @@
+    Program 2:
+	 
+Enter size of array a : 4
+Enter array elements : 1
+2
+4
+5
+Enter size of array b : 5
+Enter array elements : 2
+5
+6
+1
+8
+
+Array a : 1 2 4 5 
+Array b : 1 2 5 6 8 
+
+
+PROGRAM 3:
+
+Enter the number of elements in the stack: 3
+*********Stack operations using array*********
+----------------------------------------------
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :1
+Enter the value:2
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :1
+Enter the value:3
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :1
+Enter the value:5
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :3
+5
+
+3
+
+2
+
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :2
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :3
+3
+
+2
+
+
+1.Push
+2.Pop
+3.Show
+4.Exit
+ Enter your choice :4
+Exiting....
+
+PROGRAM 3:
+
+Enter the size of QUEUE : 4
+
+ QUEUE OPERATIONS USING ARRAY
+1.insert an element
+2.Delete an element
+3.Display the queue
+4.Exit
+Enter your choice : 1
+
+Enter the element
+3
+
+Value inserted 
+Enter your choice : 1
+
+Enter the element
+5
+
+Value inserted 
+Enter your choice : 1
+
+Enter the element
+7
+
+Value inserted 
+Enter your choice : 3
+
+ Elements in the queue are
+
+3
+5
+7
+Enter your choice : 2
+
+value deleted 
+Enter your choice : 3
+
+ Elements in the queue are
+
+5
+7
+Enter your choice : 4
+
+	
